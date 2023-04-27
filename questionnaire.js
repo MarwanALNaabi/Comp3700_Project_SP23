69558181837e2b18f9ad3e463facc0e3fd212e97
MarwanALNaabi <94378525+MarwanALNaabi@users.noreply.github.com>
2023-04-27 20:37:37 +0400

    Update questionnaire.js

--- questionnaire.js
+++ questionnaire.js
@@ -1,23 +1,81 @@
-function validateForm() {
-    let experience = document.forms["form"]["experience"].value;
-    let feedback = document.forms["form"]["feedback"].value;
-    let suggestions = document.forms["form"]["suggestions"].value;
-    alert("gellow")
+function validateForm() {    
+    let experience = document.forms["form"]["experience"];
+    let feedback = document.forms["form"]["feedback"];
+    let suggestions = document.forms["form"]["suggestions"];
+    let email = document.forms["form"]["email"];
+    let select = document.forms["form"]["select"];
+    let name = document.forms["form"]["name"];
+
+    if ((name.value)== ""){
+        alert("Fill name field");
+        name.focus();
+       
+        return false;
+    }
+    if (name.value.trim().search(/[A-Za-z_]/)==-1) {
+        alert("Invalid Name");
+        name.focus();
+        return false;
+    }
+    if ((email.value)== ""){
+        alert("Fill email field");
+        email.focus();
+       
+        return false;
+    }
+  
+    if (email.value.trim().search(/s\d{6}@student.squ.edu.om/)==-1 && email.value.trim().search(/\w+@\w+.com/)==-1) {
+        alert("Invalid Email");
+        email.focus();
+        return false;
+    }
     
 
-    if (experience.value.search(/\d{1}/)==-1){
-        alert("Write only one word");
+    if(select.selectedIndex==""){
+    alert ( "Please select a page!");
+    select.focus();
+    return false;
+    }
+
+    if ((experience.value)== ""){
+        alert("Fill website experience field");
+        experience.focus();
+       
         return false;
     }
-    if (feedback.value.search(/\w{1,500}/)== -1){
-        alert("Write between 1 and 500 words");
+    if (experience.value.trim().split(" ").length!=1){
+        alert("Write only one word in your experience about the website field");
+        experience.focus();
+        return false;
+    }
+
+
+
+    if ((feedback.value)== ""){
+        alert("Fill feedback field");
+        feedback.focus();
         return false;
 
     }
-    if (suggestions.value.search(/\w{1,1000}/) ==-1){
-        alert("Write between 1 and 1000 words");
+    if (1>=feedback.value.trim().split(" ").length || feedback.value.trim().split(" ").length >500){
+        alert("Write between 2 and 500 words feedback field");
+        feedback.focus();
         return false;
 
     }
 
+
+    if((suggestions.value)== ""){
+        alert("Fill suggestions field");
+        suggestions.focus();
+        return false;
+
+    }
+    if (1>=suggestions.value.trim().split(" ").length || suggestions.value.trim().split(" ").length >500){
+        alert("Write between 2 and 1000 words suggestions field");
+        suggestions.focus();
+        return false;
+
+    }
+    return true;
   }
