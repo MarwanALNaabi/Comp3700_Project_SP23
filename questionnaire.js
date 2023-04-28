5a3ccb526b2280a93b6d9f0d59daa0e4c7980546
MarwanALNaabi <Marwan.yn71@gmail.com>
2023-04-28 23:55:29 +0400

    changes

--- questionnaire.js
+++ questionnaire.js
@@ -1,4 +1,5 @@
 function validateForm() {    
+    // Set wanted filed in variables to be validated
     let experience = document.forms["form"]["experience"];
     let feedback = document.forms["form"]["feedback"];
     let suggestions = document.forms["form"]["suggestions"];
@@ -6,43 +7,50 @@ function validateForm() {
     let select = document.forms["form"]["select"];
     let name = document.forms["form"]["name"];
 
+    //if the name is null, send a message, focus on the field and return false.
     if ((name.value)== ""){
         alert("Fill name field");
         name.focus();
-       
         return false;
     }
+    //validate name to have only character and underscore.
     if (name.value.trim().search(/[A-Za-z_]/)==-1) {
         alert("Invalid Name");
         name.focus();
         return false;
     }
+    
+    //if the name is null, send a message, focus on the field and return false.
     if ((email.value)== ""){
         alert("Fill email field");
         email.focus();
        
         return false;
     }
-  
+
+    //validate email to have only the regular email and squ student email.
+
     if (email.value.trim().search(/s\d{6}@student.squ.edu.om/)==-1 && email.value.trim().search(/\w+@\w+.com/)==-1) {
         alert("Invalid Email");
         email.focus();
         return false;
     }
     
-
+    //if the select is null, send a message, focus on the field and return false.
     if(select.selectedIndex==""){
     alert ( "Please select a page!");
     select.focus();
     return false;
     }
 
+    //if the experience is null, send a message, focus on the field and return false.
     if ((experience.value)== ""){
         alert("Fill website experience field");
         experience.focus();
-       
         return false;
     }
+
+    // experience must have only one value
     if (experience.value.trim().split(" ").length!=1){
         alert("Write only one word in your experience about the website field");
         experience.focus();
@@ -50,13 +58,15 @@ function validateForm() {
     }
 
 
-
+    //if the feedback is null, send a message, focus on the field and return false.
     if ((feedback.value)== ""){
         alert("Fill feedback field");
         feedback.focus();
         return false;
 
     }
+
+    // feedback must have between two and 500 value
     if (1>=feedback.value.trim().split(" ").length || feedback.value.trim().split(" ").length >500){
         alert("Write between 2 and 500 words feedback field");
         feedback.focus();
@@ -65,12 +75,15 @@ function validateForm() {
     }
 
 
+    //if the suggestion is null, send a message, focus on the field and return false.
     if((suggestions.value)== ""){
         alert("Fill suggestions field");
         suggestions.focus();
         return false;
 
     }
+    
+    // feedback must have between two and 500 value
     if (1>=suggestions.value.trim().split(" ").length || suggestions.value.trim().split(" ").length >500){
         alert("Write between 2 and 1000 words suggestions field");
         suggestions.focus();
