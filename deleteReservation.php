c23745972a5cb07f0d86fb422dd10eca5bd32436
MarwanALNaabi <94378525+MarwanALNaabi@users.noreply.github.com>
2023-06-20 01:31:07 +0400

    Update deleteReservation.php

--- deleteReservation.php
+++ deleteReservation.php
@@ -3,9 +3,9 @@
 $bookNum = $_POST['deleteReservation'];
 
 $servername = "localhost";
-$username = "id20504934_webproject";
-$password = "Webproject@2023";//
-$dbname = "id20504934_sportfieldbooking";
+$username = "";
+$password = "";
+$dbname = "";
 //create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
